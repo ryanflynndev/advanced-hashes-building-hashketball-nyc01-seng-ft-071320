7af387591b71c28c2df7cae586fb277d2ef929b7
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-10 15:54:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,11 @@
 # Write your code here!
 def game_hash
-  hashketball = {:home => {:team_name => {}, :colors => {}, :players => {}}, :away => {}}
+  hashketball = {:home => 
+  {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => 
+  [
+    {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}
+    
+    ], 
+  
+  :away => {}}
 end
