1696a880d7cae675807b7ef3409582722540aa08
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-10 16:02:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,8 +13,8 @@ def game_hash
   :away => {
   {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"], :players => 
   [
-    {:player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
-    {:player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
+    {:player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0},
+    {:player_name => "", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
     {:player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15},
     {:player_name => "Mason Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5},
     {:player_name => "Jason Terry", :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}
